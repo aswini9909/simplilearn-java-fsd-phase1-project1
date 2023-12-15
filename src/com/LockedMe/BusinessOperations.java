2f53c443bb0e4d23c5abcaf768a372ad6bb1ff77
aswini <aswini5427@gmail.com>
2023-12-15 08:33:15 +0000

    added

---
+++ src/com/LockedMe/BusinessOperations.java
@@ -0,0 +1,25 @@
+package com.LockedMe;
+
+public class BusinessOperations {
+
+	public static void createFile(String fileCreate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void deleteFile(String fileDelete) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void searchFile(String fileSearch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void listFiles() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
